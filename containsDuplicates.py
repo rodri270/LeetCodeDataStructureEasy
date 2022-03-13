41d9ca88ec48c4f19b83f06beb91c385545c3a1d
Alexander Rodriguez <30503623+rodri270@users.noreply.github.com>
2022-03-12 22:46:29 -0500

    first commit

---
+++ containsDuplicates.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+'''
+class Solution:
+    nums=()
+    def containsDuplicate(self, nums) -> bool:
+        self.nums = nums
+        if len(nums) != len(set(nums)):
+           print("True")
+        else:
+            print("False")
+        
+obj = Solution()
+'''
+
+#Solution
+def containsDuplicate(nums) -> bool:
+        if len(nums) != len(set(nums)):
+           print("True")
+        else:
+            print("False")
+
+testing=(1,2,3,1)
+containsDuplicate(testing)
+
+
