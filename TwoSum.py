ebb1e7b6f84746414d00bdb2047f9c0f3b8e6eb8
rodri270 <rodri270@go.stockton.edu>
2022-03-16 11:14:25 -0400

    Final Changes to TwoSum and Started MergeSorted

--- TwoSum.py
+++ TwoSum.py
@@ -7,7 +7,6 @@ def twoSum(nums, target):
     for i in range(len(nums)):
         for b in range(i+1, len(nums)):
             if nums[i] + nums[b] == target:
-                print(nums[i],nums[b])
-                return nums[i],nums[b]
-inputs=(2,7,11,15)
+                return i,b
+inputs=[2,7,11,15]
 twoSum(inputs,9)
